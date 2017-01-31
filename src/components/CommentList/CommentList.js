67090c117fea7a7ddeb39d52c9a104d6b1587305
jhson <jhson@encar.com>
2017-01-31 18:13:17 +0900

    add async connetion

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -3,12 +3,20 @@ import {Comment} from '../';
 
 import './CommentList.css';
 
-const CommentList = () => {
+const CommentList = ({comments}) => {
+    const commentList = comments.map(
+        (comment, index) => (
+            <Comment
+                name={comment.email.split('@')[0]}
+                body={comment.body}
+                key={index}
+            />
+        )
+    );
+    
     return (
         <ul className="CommentList">
-            <Comment/>
-            <Comment/>
-            <Comment/>
+            {commentList}
         </ul> 
     );
 };
